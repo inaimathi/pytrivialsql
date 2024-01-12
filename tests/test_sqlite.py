210049c62ba24147160a3247f2ea3f18a950eced
inaimathi <leo.zovic@gmail.com>
2024-01-12 17:28:28 -0500

    db -> sqlite

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -1,18 +1,18 @@
 import tempfile
 import unittest
 
-from src.pytrivialsql import db
+from src.pytrivialsql import sqlite
 
 
 class TestDBInteraction(unittest.TestCase):
     def test_basic_interactions(self):
         with tempfile.NamedTemporaryFile(suffix=".db") as f:
-            sqlite = db.Sqlite3(f.name)
-            self.assertTrue(sqlite.create("a_table", [
+            db = sqlite.Sqlite3(f.name)
+            self.assertTrue(db.create("a_table", [
                 "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
                 "a_column TEXT", "a_number_column INTEGER",
                 "a_boolean_column INTEGER",
                 "created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL"
             ]))
-            rwid = sqlite.insert("a_table", a_column="A Value", a_number_column=42, a_boolean_column=True)
-            rw = sqlite.select("a_table", "*", where={"id": rwid})
+            rwid = db.insert("a_table", a_column="A Value", a_number_column=42, a_boolean_column=True)
+            rw = db.select("a_table", "*", where={"id": rwid})
